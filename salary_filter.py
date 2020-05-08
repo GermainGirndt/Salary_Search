c26cbf609658fe938cd5a843dc8652f8a6c93cd2
GermainPereira <pereira.germain@outlook.com>
2020-05-08 17:02:10 -0300

    Improved printed messages

--- salary_filter.py
+++ salary_filter.py
@@ -18,11 +18,11 @@ class SalarySearcher():
 
     def get_country_digit(self):
         valid_options = ["1", "2", "3"]
-        get_option_text = "Choose the country to search:\n" \
+        get_option_text = "\nChoose the country to search:\n" \
                           "1 - Germany\n" \
                           "2 - Switzerland\n" \
-                          "3 - Austria\n" \
-                          ""
+                          "3 - Austria\n\n" \
+                          "User Input: "
         country_digit = input(get_option_text)
         while country_digit not in valid_options:
             print("Invalid input. Please enter a digit from 1 to 3.")
@@ -30,8 +30,12 @@ class SalarySearcher():
         self.country_digit = country_digit
 
     def get_income_range(self):
-        text_min = "Enter the minimal salary: \n"
-        text_max = "Enter the maximal salary: \n"
+        text_min = "\n----------\n" \
+                   "Enter the minimal salary (EUR): \n\n" \
+                   "User Input: "
+        text_max = "\n----------\n" \
+                   "Enter the maximal salary (EUR): \n\n" \
+                   "User Input: "
 
         self.min_salary = int(self.get_income_range_input(text_min))
         self.max_salary = int(self.get_income_range_input(text_max))
@@ -53,7 +57,7 @@ class SalarySearcher():
         self.link = DICTIONARY_DIGIT_COUNTRY[self.country_digit]
 
     def find_all_jobs_in_link(self):
-        print("\nSearching for jobs...\n")
+        print("\n\nSearching for jobs...\n")
         response = requests.get(self.link)
         soup = BeautifulSoup(response.text, 'html.parser')
         self.jobs = soup.find_all(class_='job col odd')
@@ -80,10 +84,13 @@ class SalarySearcher():
         self.job_title_and_salary.sort(key=lambda x: int(x[1]), reverse=True)
 
     def print_job_list_with_index(self):
+        print("\n----------\n"
+              "Jobs Found:\n\n")
         for index, job in enumerate(self.job_title_and_salary, start=1):
             job_title = job[0]
             job_salary = job[1]
             print(f"{index}) {job_title} - {job_salary},00 EUR")
+        print("")
 
 
 s = SalarySearcher()
